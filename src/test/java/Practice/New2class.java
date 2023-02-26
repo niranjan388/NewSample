b6cac581e576221cf7cea19e7d3317f8c0562c08
niranjan388 <niranjanhm44@gmail.com>
2023-02-26 17:52:00 +0530

    Updated

--- src/test/java/Practice/New2class.java
+++ src/test/java/Practice/New2class.java
@@ -5,8 +5,15 @@ public class New2class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hiii");
+
 		
- /// hellow
+
+
+		//neww maga
+
+
+
+
 	}
 
 }
